8d53d80472d6c2699eeba25f51d73183672d66ec
Farhan Sadid Zihan <101565227+FarhanZihan@users.noreply.github.com>
2023-04-03 16:30:59 +0600

    Add files via upload

---
+++ No_1_JavaScript/Program_1_JavaScript_Hacks.js
@@ -0,0 +1,24 @@
+console.log("Hello World")
+// || let, var, const ||
+let lett = 34 
+var varriable = undefined
+const constant = null
+
+// || Primitive Data Types || N(null) N(number) S(string) S(symbol) B(boolean) B(BigInt) U(undefined) ||
+let a = null  // Null
+let b = 345  // Number
+let c = "Zihan"  // String
+let d = Symbol("I am Zihan") // Symbol
+let e = true  // Boolean
+let f = BigInt("3443") // BigInt
+let g // = undefined // Undefined
+
+// || Non-Primitive Data Type || Object ||
+const item = {
+    "Zihan" : 100,
+    "Harry" : 100,
+    "Aman" : 100
+}
+console.log(a, b, c, d, e)
+console.log(typeof d)
+console.log(item)
