2d968355d40ed88da3c976a954ebd509f110a9d9
Farhan Sadid Zihan <101565227+FarhanZihan@users.noreply.github.com>
2023-04-04 15:19:39 +0600

    Add files via upload

---
+++ No_1_JavaScript/Program_5_Data_Structures.js
@@ -0,0 +1,11 @@
+// || String ||
+let nam = "Zihan";
+console.log(nam.length);  // Returns length of the string
+console.log(nam[0]);  // Gives the 0th char of the string
+
+// Template Literals
+let sentence = `This is a "Competitive Programmer", 'Zihan' is his name`;  // With backtics single and double quotes can be used in a string
+console.log(sentence)
+let zihan = `Who is Zihan? ${sentence}!`;  // String Interpolation
+console.log(zihan)
+
