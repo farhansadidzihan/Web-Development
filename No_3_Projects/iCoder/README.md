a2046959d651aa348da40712f90912185c5617ab
Farhan Sadid Zihan <101565227+FarhanZihan@users.noreply.github.com>
2023-05-12 16:18:26 +0600

    Delete No_3_Projects/iCoder directory

--- No_3_Projects/iCoder/README.md
+++
@@ -1,1 +0,0 @@
-# This is a simple website using Bootstrap
--- No_3_Projects/iCoder/js/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll('.btn').forEach(buttonElement => {
-    const button = bootstrap.Button.getOrCreateInstance(buttonElement)
-    button.toggle()
-  })
