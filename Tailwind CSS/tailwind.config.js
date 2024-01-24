baaf6c4abe4cc2811211625b147ebcd4effd1261
Farhan Sadid Zihan <101565227+farhansadidzihan@users.noreply.github.com>
2024-01-24 15:18:35 +0600

    Add files via upload

---
+++ Tailwind CSS/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing: {
+        13 : "13px",
+        100 : "100px"
+      }
+    },
+  },
+  plugins: [],
+}
+
